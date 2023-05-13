f864934f8b9511ebd07cd89d46f8e82ed3280e36
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2023-05-13 09:54:34 +0200

    ci(no-task): intial setup of cypress and currents

---
+++ currents.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+   // projectId: "Ij0RfK", // Project Id obtained from https://app.currents.dev or Sorry Cypress
+   // recordKey: "XXXXXXX", // Record key obtained from https://app.currents.dev, any value for Sorry Cypress
+    cloudServiceUrl: "https://cy.currents.dev", // Sorry Cypress users - the director service URL
+/*
+    e2e: {
+        batchSize: 3, // orchestration batch size for e2e tests (Currents only, read below)
+    },
+    component: {
+        batchSize: 5, // orchestration batch size for component tests (Currents only, read below)
+    },*/
+};
