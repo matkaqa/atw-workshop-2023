06e19d846e1d79dea097e891e8450bdb1455767e
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2023-05-14 11:46:26 +0200

    bugfix: changed spec pattern to catch all js files in integration

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/integration/**/*.js',
     baseUrl: "https://displate.com",
     setupNodeEvents(on, config) {
       return cloudPlugin(on, config);
